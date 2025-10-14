af6d440140e61281f6793a3237b0e873996cf581
Nearo007 <araujo8166@gmail.com>
2025-10-14 20:33:06 -0300

    unfinished assignments crud

---
+++ controllers/assignments_controller.py
@@ -0,0 +1,26 @@
+from flask import render_template, request, redirect, url_for, Blueprint
+from models import db
+from models.assignments import Assignment
+from datetime import datetime
+#from models.skill import Skill
+
+assignment_bp = Blueprint('assignment_bp', __name__)
+
+@assignment_bp.route('/assignment_add', methods=['GET', 'POST'])
+def add_role():
+    if request.method == 'POST':
+        name = request.form['name']
+        grade_worth = request.form['grade_worth']
+        due_date_string = request.form['due_date']
+        due_date = datetime.strptime(due_date_string, '%Y-%m-%d').date()
+
+        # TODO skill
+        #skill = Skill(name="java", level=5)
+
+        new_assignment = Assignment(name=name, grade_worth=grade_worth, due_date=due_date)
+        db.session.add(new_assignment)
+        db.session.commit()
+
+        return redirect("/")
+
+    return render_template("add_assignment.html")
--- models/assignments.py
+++ models/assignments.py
@@ -1,13 +1,13 @@
 from . import db
-from .associations import role_skills
+#from .associations import role_skills
 
-class Assignments(db.Model):
+class Assignment(db.Model):
     __tablename__ = 'assignments'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     grade_worth = db.Column(db.Integer, nullable=True)
     due_date = db.Column(db.DateTime, nullable=True)
-    
+
     # relação invertida com a tabela de roles
     #roles = db.relationship('Role', secondary=role_skills, back_populates='skills')
